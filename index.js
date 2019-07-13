25e23758c056c4b4598dc62e7d94fe17165fa9f2
Jakkirt Namawang <rebornza003@hotmail.com>
2019-07-13 13:59:30 +0700

    hello github

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Jakkirt Namawang'))
+app.get('/about', (req, res) => res.send('หน้า about'))
+app.get('/contact', (req, res) => res.send('่ติดต่อเรา'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
